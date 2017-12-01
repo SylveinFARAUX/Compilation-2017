a7b4f427b96581d62642c881c2e69c41bebb32eb
rubayiza <rubayi2@gmail.com>
2017-12-01 15:40:16 +0100

    j'ai enlevé les definitions des fonctions qui se trouvaient dans un fichier . h je les ai mis dans un fichier .c

--- table_symboles_fonctions.h
+++ table_symboles_fonctions.h
@@ -13,56 +13,23 @@
  * @brief initialise la table des symboles
  * @return Pointeur sur la table des symboles initialisée
  */
-static TableDesSymboles * const init_table()
-{
-	TableDesSymboles *liste = malloc(sizeof(*liste));
-	
-    if (liste == NULL)
-	{
-		printf("init_table : Impossible d'allouer de la memoire a la table des symboles\n");
-		exit(EXIT_FAILURE);
-	}
-	liste->premier = NULL;
-    liste->taille = 0;
+static TableDesSymboles * const init_table();
 
-	return liste;
-}
 
 /**
  * @brief crée un nouveau temporaire dans la table des symboles
  * @param tds Pointeur sur la table des symboles
- * @param v Valeur du temporaire créé 
+ * @param v Valeur du temporaire créé
  */
-int new_temp(TableDesSymboles *tds, double v)
-{
-	int nDigits = floor(log10(abs(tds->taille))) + 1;
-	char taille[nDigits+1];
-	sprintf(taille, "%d", tds->taille);
-	
-	//creation du mot final
-	char buffer[5+nDigits];
-	strcpy(buffer, "temp");
-	strcat(buffer, taille);
-	
-	Symbole sb;
-	sb.nom = strdup(buffer);
-	sb.isConstant = True;
-	sb.valeur = v;
+int new_temp(TableDesSymboles *tds, double v);
 
-	push_back(tds, sb);
-	return tds->taille-1;
-}
 
 /**
  * @brief Ajoute un symbole dans la table des symboles
  * @param tds Pointeur sur la table des symboles
  * @param s Symbole à ajouter
  */
-int add(TableDesSymboles *tds, Symbole s)
-{
-	push_back(tds, s);
-	return tds->taille-1;
-}
+int add(TableDesSymboles *tds, Symbole s);
 
 /**
  * @brief Recherche d'un symbole dans la table des symboles
@@ -70,164 +37,27 @@ int add(TableDesSymboles *tds, Symbole s)
  * @param name Nom du symbole à chercher
  * @return Pointeur sur le symbole trouvé (sinon pointeur sur NULL)
  */
-Symbole * get_symbol(TableDesSymboles *tds, char * name)
-{
-	if (tds == NULL)
-	{
-		printf("toString : La table des symboles n'existe pas\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	Element *elem = tds->premier;
-	
-	while (elem != NULL && strcmp(name, elem->sb.nom) != 0)
-        elem = elem->suivant;
-	
-	
-	if (elem == NULL)
-		return NULL;
-	else
-		return &elem->sb;
-}
-
+Symbole * get_symbol(TableDesSymboles *tds, char * name);
 /**
  * @brief Recherche d'un symbole dans la table des symboles
  * @param tds Pointeur sur la table des symboles
  * @param id Indice du symbole à chercher
  * @return Pointeur sur le symbole trouvé (sinon pointeur sur NULL)
  */
-Symbole * get_symbol_by_id(TableDesSymboles *tds, int id)
-{
-	if (tds == NULL)
-	{
-		printf("toString : La table des symboles n'existe pas\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	Element *elem = tds->premier;
-	
-	while (elem != NULL && id != elem->sb.indice)
-        elem = elem->suivant;
-	
-	
-	if (elem == NULL)
-		return NULL;
-	else
-		return &elem->sb;
-}
+Symbole * get_symbol_by_id(TableDesSymboles *tds, int id);
 
-Symbole * next_symbol(TableDesSymboles *tds, Symbole * sb)
-{
-	if (tds == NULL || sb == NULL)
-	{
-		printf("toString : La table des symboles ou le symbole n'existe pas\n");
-		exit(EXIT_FAILURE);
-	}
-
-	Element *elem = tds->premier;
-	while (&elem->sb != sb && elem != NULL)
-		elem = elem->suivant;
-	if (elem == NULL)
-		return NULL;
-	else
-		return &elem->suivant->sb;
-}
+Symbole * next_symbol(TableDesSymboles *tds, Symbole * sb);
 /**
  * @brief Affiche le contenu de la table des symboles
  * @param tds Pointeur sur la table des symboles
  */
-void show_table(TableDesSymboles *tds)
-{
-	if (tds == NULL)
-	{
-		printf("toString : La table des symboles n'existe pas\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	Element *elem = tds->premier;
-	printf("La table des symboles contient %d elts :\nnom\tindice\tconstance\tvaleur\n", tds->taille);
-	
-	while (elem != NULL)
-	{
-		char *cons;
-        if (elem->sb.isConstant == True)
-			cons= "constant";
-		else
-			cons = "variable";
-
-        printf("%s\t%d\t%s\t%f\n", elem->sb.nom, elem->sb.indice, cons, elem->sb.valeur);
-        elem = elem->suivant;
-	}
-}
+void show_table(TableDesSymboles *tds);
 
 /* Fonctions internes */
-void push_front(TableDesSymboles *tds, Symbole s)
-{
-	Element *nouveau = malloc(sizeof(*nouveau));
-	
-	if (tds == NULL || nouveau == NULL)
-	{
-		printf("push_front : Impossible d'allouer de la memoire a la table des symboles\n");
-		exit(EXIT_FAILURE);
-	}
-	
-    nouveau->sb = s;
-    nouveau->suivant = NULL;
-    nouveau->sb.indice = tds->taille;
-	
-	if (tds->premier == NULL) //Si la table des symbooles ne contient pas encore d'element
-		tds->premier = nouveau;
-	else
-	{
-		nouveau->suivant = tds->premier;
-		tds->premier = nouveau;
-	}
-	
-	tds->taille++;
-}
+void push_front(TableDesSymboles *tds, Symbole s);
 
-void push_back(TableDesSymboles *tds, Symbole s)
-{
-	Element *nouveau = malloc(sizeof(*nouveau));
-	
-	if (tds == NULL || nouveau == NULL)
-	{
-		printf("push_back : Impossible d'allouer de la memoire a la table des symboles\n");
-		exit(EXIT_FAILURE);
-	}
-	
-    nouveau->sb = s;
-    nouveau->suivant = NULL;
-    nouveau->sb.indice = tds->taille;
+void push_back(TableDesSymboles *tds, Symbole s);
 
-    if (tds->taille == 0)
-		tds->premier = nouveau;
-	else if (tds->taille == 1)
-		tds->premier->suivant = nouveau;
-    else if (tds->taille > 1)
-	{
-        Element *elem = tds->premier;
-		
-		while(elem->suivant != NULL)
-			elem = elem->suivant;
-		
-        elem->suivant = nouveau;
-    }
-    tds->taille = tds->taille + 1;
-}
+void erase_first(TableDesSymboles *tds);
 
-void erase_first(TableDesSymboles *tds)
-{
-	if (tds == NULL)
-		exit(EXIT_FAILURE);
-	
-	if (tds->premier != NULL)
-	{
-		Element *aSupprimer = tds->premier;
-		tds->premier = tds->premier->suivant;
-		
-		free(aSupprimer);
-		tds->taille--;
-	}
-}
 #endif
