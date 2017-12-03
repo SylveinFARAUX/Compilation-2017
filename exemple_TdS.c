6b1d941cbe21d7e89a2c3de3a71b5296b7a193b4
sfaraux <sylveinfaraux@hotmail.com>
2017-12-03 23:45:00 +0100

    rédaction d'une méthode pour générer du code mips à partir des quads (pour l'instant seules les opérations CREATEVAR et PLUS sont implémentés) ->generercodemips() le fichier de sortie ex.s peut être chargé avec l'interpreteur MIPS MARS4_5

--- exemple_TdS.c
+++ exemple_TdS.c
@@ -2,38 +2,91 @@
 #include "table_symboles_fonctions.h"
 #include "liste_quad_fonctions.h"
 
-void affichercodemips(TableDesSymboles *tds)
+
+void generercodemips(TableDesSymboles *tds, ListeQuad *lq)
 {
-	/*zone code du code mips*/
-	Symbole *s = &tds->premier->sb;
-	printf("\n\n");
-	printf(".text\nmain:\n");
-	while (s != NULL)
-	{
-		printf("li $v0,3\nl.s $f12,%s\nsyscall\n", s->nom);
-		s = next_symbol(tds, s);
+	FILE *fichier_mips = NULL;
+	fichier_mips = fopen("ex.s", "w+");
+	
+	if (fichier_mips == NULL){
+		printf("Erreur lors de l'ouverture de ex.spi\n");
+		return;
 	}
-
-	//affichage de program terminated
-	printf("li $v0,4\nla $a0,str1\nsyscall\n");
-	printf("li $v0,4\nla $a0,strt\nsyscall\n"); 
-	printf("li $v0,4\nla $a0,str1\nsyscall\n");
-
-	printf("# exit\nli $v0,10        # on sort du programme proprement\nsyscall\n");
-	/*li $v0, 3         # appel système pour afficher un réel
-  	l.s $f12,temp0   # chargement du réel à afficher
-  	syscall          # affichage*/
-		
+	
+	QElement *elem = lq->premier;
+	Quad *dataQuad = &elem->q;
 	
 	/*zone data du code mips*/
-	s = &tds->premier->sb;
-	printf(".data\n");
-	while (s != NULL)
+	fprintf(fichier_mips, ".data\n");
+	while (dataQuad != NULL)
 	{
-		printf("%s: .double %f\n", s->nom, s->valeur);
-		s = next_symbol(tds, s);
+		if (dataQuad->operateur == CREATEVAR)//si le quad demande la création d'une variable
+		{
+			//on recherche le symbole dans la table des symboles
+			Symbole *trouve = get_symbol(tds, dataQuad->arg1);
+			
+			if (trouve != NULL)//Si on a trouvé le symbole en question
+				fprintf(fichier_mips, "\t%s:\t.float %f  #chargement du symbole %s\n", trouve->nom, trouve->valeur, trouve->nom);
+			else
+				printf("Erreur : symbole %s non référencé dans la table des symboles\n", dataQuad->arg1);
+		}
+		elem = elem->suivant;
+		dataQuad = &elem->q;
 	}
-	printf("str1:  .asciiz \"\\n\"\nstr2:  .asciiz \" \"\nstrt:  .asciiz \"program terminated\"\n");
+	fprintf(fichier_mips, "\tnewline:\t.asciiz \"\\n\"\n\tspace:\t.asciiz \" \"\n\ttermin:\t.asciiz \"program terminated\"\n");
+	
+	/*zone intructions du code mips (calcul arithmetique)*/
+	elem = lq->premier;
+	dataQuad = &elem->q;
+	fprintf(fichier_mips, "\n\n");
+	fprintf(fichier_mips, ".text\n\tmain:\n");
+	show_table(tds);
+
+	while (dataQuad != NULL)
+	{
+		if (dataQuad->operateur == PLUS)//Si le quad demande de faire une addition
+		{
+			//on recherche les symboles dans la table des symboles
+			Symbole *op1 = get_symbol(tds, dataQuad->arg1);
+			Symbole	*op2 = get_symbol(tds, dataQuad->arg2);
+			Symbole	*res = get_symbol(tds, dataQuad->result);
+			if (op1 == NULL)printf("%s est NULL !\n", dataQuad->arg1);
+			if (op2 == NULL)printf("%s est NULL !\n", dataQuad->arg2);
+			if (res == NULL)printf("%s est NULL !\n", dataQuad->result);
+			if (op1 != NULL && op2 != NULL && res != NULL)
+			{
+				//fprintf(fichier_mips, "lw $t0, %s", "li $v0,3\nl.s $f12,%s\n", nom);
+				fprintf(fichier_mips, "\tl.s $f0, %s  #chargement du flottant dans $f0\n", op1->nom);
+				fprintf(fichier_mips, "\tl.s $f1, %s\n", op2->nom);
+				fprintf(fichier_mips, "\tadd.s $f2, $f0, $f1  #addition de flottants, $f2 <- $f0 + $f1\n");
+				fprintf(fichier_mips, "\ts.s $f2, %s  #store simple float in the %s variable\n", res->nom, res->nom);
+				
+				//Affichage du resultat
+				fprintf(fichier_mips, "\n\tli $v0, 2  #appel systeme pour printer un simple flottant\n");
+				fprintf(fichier_mips, "\tl.s $f12, %s  #chargement dans f12(le parametre) l'adresse du reel à printer\n", res->nom);
+				fprintf(fichier_mips, "\tsyscall\n");//je submit au système
+				
+				//retour à la ligne
+				fprintf(fichier_mips, "\tli $v0,4\n\tla $a0,newline\n\tsyscall\n");
+			}
+			else
+				printf("Erreur : addition avec des symboles qui n'existent pas");
+		}
+		
+		elem = elem->suivant;
+		dataQuad = &elem->q;
+	}
+	
+	//affichage de program terminated
+	fprintf(fichier_mips, "\n\t#affichage de \"program terminated\"\n");
+	fprintf(fichier_mips, "\tli $v0,4\n\tla $a0,newline\n\tsyscall\n");
+	fprintf(fichier_mips, "\tli $v0,4\n\tla $a0,termin\n\tsyscall\n"); 
+	fprintf(fichier_mips, "\tli $v0,4\n\tla $a0,newline\n\tsyscall\n");
+
+	fprintf(fichier_mips, "\n\t#exit\n\tli $v0,10  # on sort du programme proprement\n\tsyscall\n");
+	
+	if (fclose(fichier_mips) != 0)
+		printf("Erreur lors de la fermeture du fichier ex.spi\n");
 }
 
 int main(){
@@ -82,8 +135,7 @@ int main(){
 	//Quad de calcul
 	genquad(lq, PLUS, a.nom, temp->nom, ptr->nom, NULL);
 	
-	qshow_liste(lq);
-	
-	
+	//génère le code mips dans le fichier ex.s, fichier chargeable avec l'interpreteur MARS4_5 (ou QtSpim)
+	generercodemips(tds, lq);  
 	return 0;
 }
